c3238f2f262e78452d4c4eeed493a2bb9c395087
Dpec200 <144163736+Dpec200@users.noreply.github.com>
2023-09-21 17:37:10 -0400

    Add files via upload

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -101,8 +101,8 @@ def main():
             }
 
             .ag-courses_item_gris_title {
-            -ms-flex-preferred-size: calc(70% - 30px);
-            flex-basis: calc(70% - 30px);
+            -ms-flex-preferred-size: auto;
+            flex-basis: auto;
             height: auto;
             weight: auto;
 
@@ -504,7 +504,7 @@ def main():
         
         if (len(df_oferta_snackys) > 0 ):
             cliente_pec = df_oferta_snackys['clientName'].unique().tolist()
-            st.markdown(f'<div class="ag-format-container"><div class="ag-courses_box"><div class="ag-courses_item_gris_title"><div href="#" class="ag-courses-item_link_gris"><span class="adjustable-text" style="font-size: 70px;">Dashboard de Ofertas</span><span class="adjustable-text" style="font-size: 40px;">Bienvenido {cliente_pec[0]}</span></div></div></div></div></div></div>',unsafe_allow_html=True)
+            st.markdown(f'<div class="ag-format-container"><div class="ag-courses_box"><div class="ag-courses_item_gris_title"><div href="#" class="ag-courses-item_link_gris"><span class="adjustable-text" style="font-size: 70px;">Dashboard de Ofertas <br></span><span class="adjustable-text" style="font-size: 40px;">Bienvenido {cliente_pec[0]}</span></div></div></div></div></div></div>',unsafe_allow_html=True)
             # st.subheader(f"Bienvenido {cliente_pec[0]}", class_="centered-subheader")
             # st.markdown(f'<div class="ag-format-container"><div class="ag-courses_box"><div class="ag-courses_item_gris_subtitle"><div href="#" class="ag-courses-item_link_gris"><span class="adjustable-text" style="font-size: 40px;">Bienvenido {cliente_pec[0]}</span></div></div></div></div></div></div>',unsafe_allow_html=True,)
         st.write("---")
