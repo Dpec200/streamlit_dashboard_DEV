f88985eaaba2ce63dc3151d40d4111c88fd54057
Dpec200 <144163736+Dpec200@users.noreply.github.com>
2023-09-22 18:32:21 -0400

    Add files via upload

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -553,7 +553,7 @@ def main():
             total = sum(valores)
             # Colores para el gráfico
             colores = ['#2D8DEC', '#DFE2E5']
-            plt.figure(figsize=(5, 4), facecolor='none')  
+            plt.figure(figsize=(5, 3.5), facecolor='none')  
             # Crea el gráfico de torta
             plt.pie(x=valores, labels=etiquetas, colors=colores, autopct='%1.1f%%', startangle=140)
             plt.subplots_adjust(left=0.1, right=1, top=1, bottom=0.1)
