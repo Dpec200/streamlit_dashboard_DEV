b9c5bf10a1953572167c0ba8a223b83ea738fbb6
Dpec200 <144163736+Dpec200@users.noreply.github.com>
2023-09-24 19:47:23 -0400

    Add files via upload

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -698,11 +698,14 @@ def main():
 
         # Agrupar por mes y semana, contando las ocurrencias de cada categoría
         grouped = grafico_barras_data.groupby(['mes', 'semana', 'msgBody']).size().reset_index(name='count')
+        grouped1 = grouped[grouped['msgBody'] == 1]
+        grouped2 = grouped[grouped['msgBody'] == 2]
+        
         st.write(grouped)
         # Crear el gráfico de barras apiladas
         plt.figure(figsize=(10, 4))
-        sns.barplot(x='mes', y='count', hue='semana', data=grouped, color='#2D8DEC')
-        sns.barplot(x='mes', y='count', hue='semana', data=grouped, color='#DFE2E5')
+        sns.barplot(x='mes', y='count', hue='semana', data=grouped1, color='#2D8DEC')
+        sns.barplot(x='mes', y='count', hue='semana', data=grouped2, color='#DFE2E5')
         plt.xlabel('Mes')
         plt.ylabel('Valores')
         plt.title('Gráfico de Barras Apiladas por Mes y Semana')
