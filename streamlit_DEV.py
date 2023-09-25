71dd337cfc0c9c765f8d21750582bc76f35be226
Dpec200 <144163736+Dpec200@users.noreply.github.com>
2023-09-25 19:55:04 -0400

    Add files via upload

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -840,7 +840,9 @@ def main():
 
         # Agregar etiquetas a ubicaciones específicas en el eje x
         for x, etiqueta in zip(x_coords, etiquetas):
-            ax.annotate(etiqueta, (x, valores[x]), ha='center', va='bottom')
+            ax.annotate(etiqueta, (x, valores[x]), ha='center', va='top')
+            if x == 1:
+                ax.annotate('enero', (x, valores[x]), ha='center', va='top')
 
         plt.xlabel('Categorías')
         plt.ylabel('Valores')
