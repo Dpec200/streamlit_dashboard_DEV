25cb6cd9e645e68331cc7ee558ace8973395dafe
Dpec200 <144163736+Dpec200@users.noreply.github.com>
2023-09-25 20:29:20 -0400

    Add files via upload

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -806,21 +806,21 @@ def main():
             
             ax.bar(i*6 + width, grouped1['2'], width=width, label='semana 1', color='#DFE2E5', edgecolor='black')
             ax.bar(i*6 + width, grouped1['1'], bottom=grouped1['2'], width=width, label='semana 1', color='#2D8DEC', edgecolor='black')
-            ax.text(x=(i*6 + width), s='1era', y=-0.40, ha='right')
+            ax.text(x=(i*6 + width), s='1era', y=-0.40, ha='center')
 
             ax.bar(i*6 + 2*width, grouped3['2'], width=width, label='semana 2', color='#DFE2E5', edgecolor='black')
             ax.bar(i*6 + 2*width, grouped3['1'], bottom=grouped3['2'], width=width, label='semana 2', color='#2D8DEC', edgecolor='black')
-            ax.text(x=(i*6 + 2*width), s='2da', y=-0.40, ha='right')
+            ax.text(x=(i*6 + 2*width), s='2da', y=-0.40, ha='center')
 
-            ax.text(x=(i*6 + ((4*width)/2)), s=str.capitalize(mes), y=-0.8, ha='right')
+            ax.text(x=(i*6 + ((4*width)/2)), s=str.capitalize(mes), y=-0.8, ha='center')
 
             ax.bar(i*6 + 3*width, grouped5['2'], width=width, label='semana 3', color='#DFE2E5', edgecolor='black')
             ax.bar(i*6 + 3*width, grouped5['1'], bottom=grouped5['2'], width=width, label='semana 3', color='#2D8DEC', edgecolor='black')
-            ax.text(x=(i*6 + 3*width), y=-0.40, s='3era', ha='right')
+            ax.text(x=(i*6 + 3*width), y=-0.40, s='3era', ha='center')
 
             ax.bar(i*6 + 4*width, grouped7['2'], width=width, label='semana 4', color='#DFE2E5', edgecolor='black')
             ax.bar(i*6 + 4*width, grouped7['1'], bottom=grouped7['2'], width=width, label='semana 4', color='#2D8DEC', edgecolor='black')
-            ax.text(s='4ta', x=(i*6 + 4*width), y=-0.40, ha='right')
+            ax.text(s='4ta', x=(i*6 + 4*width), y=-0.40, ha='center')
             
 
         patch_1 = mpatches.Patch(color='#2D8DEC', label='Interesado')
