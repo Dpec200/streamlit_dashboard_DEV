03abaebb7ff26f16d4ce331af0203341fd5f05dd
Dpec200 <144163736+Dpec200@users.noreply.github.com>
2023-09-26 14:23:23 -0400

    Add files via upload

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -710,7 +710,7 @@ def main():
                 ax.bar(i*6*width + 2*width, grouped2['1'], bottom=grouped2['2'], width=width, label='semana 2', color='#2D8DEC', edgecolor='black')
                 ax.text(x=(i*6*width + 2*width), s='2da', y=-0.80, ha='center')
             else:
-                ax.text(x=(i*6*width + (2*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.20, ha='center')
+                ax.text(x=(i*6*width + (2*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.40, ha='center')
 
 
             if len(grouped3) > 0:
@@ -718,16 +718,16 @@ def main():
                 ax.bar(i*6*width + 3*width, grouped3['1'], bottom=grouped3['2'], width=width, label='semana 3', color='#2D8DEC', edgecolor='black')
                 ax.text(x=(i*6*width + 3*width), y=-0.80, s='3era', ha='center')
             else:
-                ax.text(x=(i*6*width + (3*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.20, ha='center')
+                ax.text(x=(i*6*width + (3*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.40, ha='center')
 
 
             if len(grouped4) > 0:
                 ax.bar(i*6*width + 4*width, grouped4['2'], width=width, label='semana 4', color='#DFE2E5', edgecolor='black')
                 ax.bar(i*6*width + 4*width, grouped4['1'], bottom=grouped4['2'], width=width, label='semana 4', color='#2D8DEC', edgecolor='black')
                 ax.text(s='4ta', x=(i*6*width + 4*width), y=-0.80, ha='center')
-                ax.text(x=(i*6*width + (5*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.20, ha='center')
+                ax.text(x=(i*6*width + (5*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.40, ha='center')
             else:
-                ax.text(x=(i*6*width + (4*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.20, ha='center')
+                ax.text(x=(i*6*width + (4*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-1.40, ha='center')
             
 
         patch_1 = mpatches.Patch(color='#2D8DEC', label='Interesado')
