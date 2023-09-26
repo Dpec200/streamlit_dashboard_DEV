1c6b7fa684efc53b4c00065d1386ee27ecc6aaa5
Dpec200 <144163736+Dpec200@users.noreply.github.com>
2023-09-26 13:59:59 -0400

    Add files via upload

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -699,6 +699,8 @@ def main():
 
             grouped4 = grouped[(grouped['semana'] == '4') & (grouped['mes'] == mes)]
             
+            meses_str = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
             if len(grouped1) > 0:
                 ax.bar(i*6*width + width, grouped1['2'], width=width, label='semana 1', color='#DFE2E5', edgecolor='black')
                 ax.bar(i*6*width + width, grouped1['1'], bottom=grouped1['2'], width=width, label='semana 1', color='#2D8DEC', edgecolor='black')
@@ -708,8 +710,10 @@ def main():
                 ax.bar(i*6*width + 2*width, grouped2['2'], width=width, label='semana 2', color='#DFE2E5', edgecolor='black')
                 ax.bar(i*6*width + 2*width, grouped2['1'], bottom=grouped2['2'], width=width, label='semana 2', color='#2D8DEC', edgecolor='black')
                 ax.text(x=(i*6*width + 2*width), s='2da', y=-0.40, ha='center')
+            else:
+                ax.text(x=(i*6*width + (3*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-0.8, ha='center')
 
-            meses_str = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+            
 
             ax.text(x=(i*6*width + (5*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-0.8, ha='center')
 
@@ -717,11 +721,15 @@ def main():
                 ax.bar(i*6*width + 3*width, grouped3['2'], width=width, label='semana 3', color='#DFE2E5', edgecolor='black')
                 ax.bar(i*6*width + 3*width, grouped3['1'], bottom=grouped3['2'], width=width, label='semana 3', color='#2D8DEC', edgecolor='black')
                 ax.text(x=(i*6*width + 3*width), y=-0.40, s='3era', ha='center')
+            else:
+                ax.text(x=(i*6*width + (4*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-0.8, ha='center')
+
 
             if len(grouped4) > 0:
                 ax.bar(i*6*width + 4*width, grouped4['2'], width=width, label='semana 4', color='#DFE2E5', edgecolor='black')
                 ax.bar(i*6*width + 4*width, grouped4['1'], bottom=grouped4['2'], width=width, label='semana 4', color='#2D8DEC', edgecolor='black')
                 ax.text(s='4ta', x=(i*6*width + 4*width), y=-0.40, ha='center')
+                ax.text(x=(i*6*width + (5*width)/2), s=str.capitalize(meses_str[int(mes)-1]), y=-0.8, ha='center')
             
 
         patch_1 = mpatches.Patch(color='#2D8DEC', label='Interesado')
