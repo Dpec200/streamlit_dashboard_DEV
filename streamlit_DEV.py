73f2f99ce5a7445f60dc72aae59f7d428ede6115
josej16 <josebrox16@gmail.com>
2023-10-04 21:38:30 -0400

    arreglo final grafico torta

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -581,7 +581,7 @@ def main():
 
             def autopct(pct, allvals):
                 absolute = int(round(pct/100*sum(allvals)))
-                return f"{pct}% ({absolute})"
+                return f"{pct:.1f}% ({absolute})"
 
             # Crea el gráfico de torta
             plt.pie(x=valores, labels=etiquetas, colors=colores, autopct=lambda pct: autopct(pct, valores), startangle=90)
