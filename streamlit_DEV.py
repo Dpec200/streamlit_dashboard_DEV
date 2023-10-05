bf4db3a2973d0425ffbffdc295636677b6dcecdd
josej16 <josebrox16@gmail.com>
2023-10-04 21:37:12 -0400

    sda

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -580,7 +580,7 @@ def main():
             plt.figure(figsize=(5, 3.5), facecolor='none')
 
             def autopct(pct, allvals):
-                absolute = int(pct/100*sum(allvals))
+                absolute = int(round(pct/100*sum(allvals)))
                 return f"{pct}% ({absolute})"
 
             # Crea el gráfico de torta
