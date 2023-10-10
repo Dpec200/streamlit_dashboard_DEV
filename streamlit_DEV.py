91bd3f3a5139ad3744f9bf082a76f5a30eb16b97
josej16 <josebrox16@gmail.com>
2023-10-10 18:59:28 -0400

    errores minimos

--- streamlit_DEV.py
+++ streamlit_DEV.py
@@ -646,13 +646,13 @@ def main():
                     #     x_val += 1
                     
                     if len(grouped1) > 0:
-                        ax.bar(x_val*6*width + width, grouped1['2'], width=width, label=f'semana {semana}', color='#DFE2E5', edgecolor='black')
-                        ax.text(x=(x_val*6*width + width),y=(int(grouped1['2'])-1) , s=str(int(grouped1['2'])), ha='center', va='bottom')
+                        ax.bar(x_val*6*width + (indice + 1)*width, grouped1['2'], width=width, label=f'semana {semana}', color='#DFE2E5', edgecolor='black')
+                        ax.text(x=(x_val*6*width + (indice + 1)*width),y=(int(grouped1['2'])-1) , s=str(int(grouped1['2'])), ha='center', va='bottom')
 
-                        ax.bar(x_val*6*width + width, grouped1['1'], bottom=grouped1['2'], width=width, label=f'semana {semana}', color='#2D8DEC', edgecolor='black')
-                        ax.text(x=(x_val*6*width + width),y=(int(grouped1['1']) + int(grouped1['2']))-1 , s=str(int(grouped1['1'])), ha='center', va='bottom')
+                        ax.bar(x_val*6*width + (indice + 1)*width, grouped1['1'], bottom=grouped1['2'], width=width, label=f'semana {semana}', color='#2D8DEC', edgecolor='black')
+                        ax.text(x=(x_val*6*width + (indice + 1)*width),y=(int(grouped1['1']) + int(grouped1['2']))-1 , s=str(int(grouped1['1'])), ha='center', va='bottom')
 
-                        ax.text(x=(x_val*6*width + width), s=f'Sem. {semana}', y=-0.80, ha='center')
+                        ax.text(x=(x_val*6*width + (indice + 1)*width), s=f'Sem. {semana}', y=-0.80, ha='center')
                         mes_no_nulo = True
                         indice_semana = indice + 1
                     else:
@@ -698,7 +698,8 @@ def main():
                 # else:
                 #     ax.text(x=(x_val*6*width + (4*width)/2), s=str.capitalize(mes), y=-1.40, ha='center')
                 #     continue
-
+                if mes_no_nulo == True:
+                    ax.text(x=(x_val*6*width + ((indice + 2)*width)/2), s=str.capitalize(mes), y=-1.40, ha='center')
 
             # ax.text(x=(i*6*width*index + (4*width)/2), s=str.capitalize(ano), y=-1.40, ha='center')
 
